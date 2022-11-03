3b341740852d867ee3526064c47757b7b79045e9
Kevin Scheffelmeier <kevin@ksch.io>
2022-11-03 12:20:57 +0100

    refactor(colorscheme): oxocarbon

--- nvim/lua/ksch/appearance/colorscheme.lua
+++ nvim/lua/ksch/appearance/colorscheme.lua
@@ -6,10 +6,11 @@
 -- vim.opt.background = "dark"
 -- vim.cmd "colorscheme tundra"
 
+-- vim.g.rasmus_transparent = true
 -- vim.g.rasmus_variant = "monochrome"
 -- vim.cmd [[colorscheme rasmus]]
 
 -- vim.cmd "colorscheme coal"
 
-vim.cmd "colorscheme oxocarbon-lua"
-vim.g.oxocarbon_lua_keep_terminal = true
+vim.cmd "colorscheme oxocarbon"
+-- vim.g.oxocarbon_lua_keep_terminal = true
