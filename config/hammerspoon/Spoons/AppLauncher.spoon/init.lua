6bcfdfdd6c08557a2b43f34571b298c074172dd3
Kevin Scheffelmeier <kevin@ksch.io>
2023-03-13 11:29:36 +0100

    chore: window focus bindings
    Signed-off-by: Kevin Scheffelmeier <kevin@ksch.io>

--- config/hammerspoon/Spoons/AppLauncher.spoon/init.lua
+++ config/hammerspoon/Spoons/AppLauncher.spoon/init.lua
@@ -14,6 +14,12 @@ function obj:start()
     hs.hotkey.bind(mash, "t", function() hs.application.launchOrFocus("Alacritty") end)
     hs.hotkey.bind(mash, "z", function() hs.application.launchOrFocus("Kaleidoscope") end)
     hs.hotkey.bind(mash, "g", function () hs.urlevent.openURL("https://mail.google.com") end)
+
+    hs.hotkey.bind(mash, "1", function() hs.application.launchOrFocus("Alacritty") end)
+    hs.hotkey.bind(mash, "2", function() hs.application.launchOrFocus("Arc") end)
+    hs.hotkey.bind(mash, "3", function() hs.application.launchOrFocus("Visual Studio Code") end)
+    hs.hotkey.bind(mash, "4", function() hs.application.launchOrFocus("Figma") end)
+    hs.hotkey.bind(mash, "9", function() hs.application.launchOrFocus("Spotify") end)
 end
 
 return obj
